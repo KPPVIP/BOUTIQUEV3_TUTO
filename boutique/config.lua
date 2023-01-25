b8303157c269d6164a6e32327e25cb8d22571e34
Guudz <Kilian1702@outlook.com>
2023-01-25 07:16:24 +0100

    Initial commit

---
+++ boutique/config.lua
@@ -0,0 +1,82 @@
+---- discord : https://discord.gg/pRXCnA8
+
+-----------------------------------------------------------------------
+
+touche_open_menu = 168 -- Touche pour ouvrir le menu ( F11 )
+
+-----------------------------------------------------------------------
+
+discord_webhook = {
+	url = "https://discord.com/api/webhooks/753806821021712445/XMsDqg-8ZPvWj_-e9_svmmFcVeDNwwtjNGKu0jj64IVIfi4HdDg-s9rgXjHdnA97iQlc"
+}
+
+----------------------------------------------------------------------- license ou steam
+
+id_system_l_o_s = "steam"
+
+----------------------------------------------------------------------- nom de la money virtuelle
+
+moneypoints = "~b~Crédit(s)"
+
+----------------------------------------------------------------------- https://wiki.rage.mp/index.php?title=Notification_Pictures
+
+img_notif = "CHAR_SOCIAL_CLUB"
+
+-----------------------------------------------------------------------
+
+customprice = 150
+
+-----------------------------------------------------------------------
+
+donner_point = true
+
+-----------------------------------------------------------------------
+
+menu_vehicule = true
+
+item_vehicule = {
+	{img = "bolide", name = "Bugatti Bolide" , model = "rmodbugatti", point = 1000, place = 2, vitesse = 450},
+	{img = "voiturenoire", name = "La Voiture Noire" , model = "rmodbolide", point = 1000, place = 2, vitesse = 449},
+	{img = "charger", name = "Dodge Charger 69" , model = "rmodcharger69", point = 1000, place = 2, vitesse = 448},
+	{img = "f12", name = "Ferrari F12 TDF" , model = "rmodf12tdf", point = 1000, place = 2, vitesse = 447},
+	{img = "f40", name = "Ferrari F40" , model = "rmodf40", point = 1000, place = 2, vitesse = 446},
+	{img = "b63s", name = "Brabus GT63S" , model = "rmodgt63", point = 1000, place = 2, vitesse = 445},
+	{img = "gtr", name = "Nissan GTR" , model = "rmodgtr50", point = 1000, place = 2, vitesse = 444},
+	{img = "jeep", name = "Jeep Rmod" , model = "rmodjeep", point = 1000, place = 2, vitesse = 443},
+	{img = "m5", name = "Bmw M5 E34" , model = "rmodm5e34", point = 1000, place = 2, vitesse = 442},
+	{img = "golf", name = "Golf 7R" , model = "rmodmk7", point = 1000, place = 2, vitesse = 441},
+	{img = "skyline", name = "Nissan Skyline R34" , model = "rmodskyline34", point = 1000, place = 2, vitesse = 440},
+	{img = "zl1", name = "Chevrolet Camaro ZL1" , model = "rmodzl1", point = 1000, place = 2, vitesse = 439}
+}   
+
+-----------------------------------------------------------------------
+
+menu_arme = true
+
+item_arme = {
+	{img = "Screenshot_127", name = "Couteau" , model = "weapon_knife", point = 10},
+    {img = "baseball-bat", name = "Bat de baseball" , model = "weapon_bat", point = 10},
+    {img = "hachet", name = "Hachette" , model = "weapon_machete", point = 10},
+    {img = "pistol", name = "Pistolet" , model = "weapon_pistol", point = 1500},
+    {img = "revolvermk2", name = "Revolver MK2" , model = "weapon_revolver_mk2", point = 2000},
+    {img = "minismg", name = "Mini-SMG" , model = "weapon_microsmg", point = 2200},
+    {img = "smgmk2", name = "Mini-SMG MK2" , model = "weapon_smg_mk2", point = 2500},
+	{img = "assault-rifle-mk2", name = "AK47" , model = "weapon_assaultrifle", point = 4200},
+	{img = "m4", name = "M4" , model = "weapon_carbinerifle", point = 4200},
+	{img = "sniper", name = "Sniper" , model = "weapon_sniperrifle", point = 5000}
+} 
+
+-----------------------------------------------------------------------
+
+
+menu_money = true
+
+item_money = {
+	{img = "money", name = "10,000$" , model = 10000, point = 500},
+	{img = "money", name = "20,000$" , model = 20000, point = 1000},
+	{img = "money", name = "45,000$" , model = 45000, point = 1500},
+	{img = "money", name = "75,000$" , model = 75000, point = 2000}
+} 
+
+
+-----------------------------------------------------------------------
